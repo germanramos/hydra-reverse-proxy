df4ae2ad4942ce2ebceec1f7b7a718764485db55
Alberto Gregoris <alberto.gregoris@intelygenz.com>
2014-09-18 10:30:02 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,4 @@
 
 . ./build
 
-go test -i ./reverse_proxy
-go test -v ./reverse_proxy
-
-go test -i ./tests/acceptance
-go test -v ./tests/acceptance
+ginkgo -r --failOnPending --cover --trace reverse_proxy/
