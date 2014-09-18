186a4f2d7e2523442215da715e761c712e33b0d9
Alberto Gregoris <alberto.gregoris@intelygenz.com>
2014-09-18 17:19:28 +0200

    Fixed bug in makeDEB.sh

--- scripts/packaging/makeDEB.sh
+++ scripts/packaging/makeDEB.sh
@@ -6,13 +6,16 @@ rm -rf ~/debbuild
 mkdir -p ~/debbuild/DEBIAN
 cp control ~/debbuild/DEBIAN
 
+mkdir -p ~/debbuild/etc
 cp ./fixtures/hydra-reverse-proxy.conf ~/debbuild/etc
 
 mkdir -p ~/debbuild/etc/init.d
 cp hydra-reverse-proxy-init.d.sh ~/debbuild/etc/init.d/hydra-reverse-proxy
 
+mkdir -p ~/debbuild/usr/local
 cp ../../bin/hydra-reverse-proxy  ~/debbuild/usr/local
 
+chmod -R 644 ~/debbuild/usr/local
 chmod 755 ~/debbuild/etc/init.d/hydra-reverse-proxy
 chmod 755 ~/debbuild/usr/local/hydra-reverse-proxy
 
